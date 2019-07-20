32b5c571c663f1a4ebd87cb3acc3fde4df84a522
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:10:37 +0900

    published

--- js/hasJQuery.js
+++ js/hasJQuery.js
@@ -2,19 +2,15 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		/**
-		 * @name hasJquery
-		 * @since 2017-12-06
-		 * @return {boolean}
-		 */
-		window.hasJquery = function() {
-			return typeof window.jQuery === 'function';
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+	/**
+	 * @name hasJquery
+	 * @since 2017-12-06
+	 * @return {boolean}
+	 */
+	window.hasJquery = function() {
+		return typeof window.jQuery === 'function';
+	};
+})();
